73705162671c0ace66055247d2697ee6dca300fb
giovixx9 <giovannimaiaru36@gmail.com>
2026-08-12 17:07:30 +0200

    Add Theme.kt

---
+++ app/src/main/java/com/giovixx9/flappybird/ui/theme/Theme.kt
@@ -0,0 +1,37 @@
+package com.giovixx9.flappybird.ui.theme
+
+import androidx.compose.foundation.isSystemInDarkMode
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.darkColorScheme
+import androidx.compose.material3.lightColorScheme
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color
+
+private val DarkColorScheme = darkColorScheme(
+    primary = Purple80,
+    secondary = PurpleGrey80,
+    tertiary = Pink80
+)
+
+private val LightColorScheme = lightColorScheme(
+    primary = Purple40,
+    secondary = PurpleGrey40,
+    tertiary = Pink40
+)
+
+@Composable
+fun FlappyBirdTheme(
+    darkTheme: Boolean = isSystemInDarkMode(),
+    content: @Composable () -> Unit
+) {
+    val colorScheme = when {
+        darkTheme -> DarkColorScheme
+        else -> LightColorScheme
+    }
+
+    MaterialTheme(
+        colorScheme = colorScheme,
+        typography = Typography,
+        content = content
+    )
+}
